4312bbb4fc449cbbd232a6b1b7c7d7c59941db93
larry1sf <larryycel@gmail.com>
2025-12-07 00:10:55 -0500

    feat: Implement initial e-commerce features including product display, categories, order forms, and Strapi integration.

--- back-models/types/generated/contentTypes.d.ts
+++ back-models/types/generated/contentTypes.d.ts
@@ -607,12 +607,15 @@ export interface ApiProductProduct extends Struct.CollectionTypeSchema {
     createdAt: Schema.Attribute.DateTime;
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
-    description: Schema.Attribute.String &
+    description: Schema.Attribute.Text &
       Schema.Attribute.Required &
       Schema.Attribute.SetPluginOptions<{
         i18n: {
           localized: true;
         };
+      }> &
+      Schema.Attribute.SetMinMaxLength<{
+        maxLength: 150;
       }>;
     images: Schema.Attribute.Media<
       'images' | 'files' | 'videos' | 'audios',
---
+++ web-models/src/services/get-productsCategories.ts
@@ -0,0 +1,7 @@
+import type { ProductAttributes, StrapiResponse } from "~/types/product"
+import { gets } from "~/services/consts"
+export async function getProductsCategories({ productCategory }: { productCategory: string }): Promise<StrapiResponse<ProductAttributes>> {
+    return await gets({
+        query: `/api/products?filters[product_category][slug][$eq]=${productCategory}&fields[0]=slug&fields[1]=name&fields[2]=description&fields[3]=price&fields[4]=isActive&fields[5]=size&fields[6]=color&fields[7]=texture&populate[images][fields][0]=url&populate[product_category][fields]=slug`
+    })
+}
