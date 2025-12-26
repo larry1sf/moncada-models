40f430806661ee926a276bdb255ff69d8ab4bc03
larry1sf <larryycel@gmail.com>
2025-12-26 00:44:24 -0500

    adapter

--- web-models/astro.config.mjs
+++ web-models/astro.config.mjs
@@ -1,7 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
-    output: "static",
+  output: "static",
+
+  adapter: node({
+    mode: "standalone",
+  }),
 });
