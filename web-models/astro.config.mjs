99e362438237c35ce502b6035d1e571d7d2f25fa
larry1sf <larryycel@gmail.com>
2025-12-31 19:17:15 -0500

    UPDATE: agregando el sitemap con la libreria astro sitemap

--- web-models/astro.config.mjs
+++ web-models/astro.config.mjs
@@ -1,16 +1,27 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://moncada-models.com', // Reemplaza con tu dominio real
   output: "static",
+
   build: {
     format: 'directory',
     inlineStylesheets: 'auto',
   },
+
   prefetch: {
     prefetchAll: true,
     defaultStrategy: 'hover'
   },
+
   compressHTML: true,
+  integrations: [
+    sitemap({
+      filter: (page) => !page.includes('/pedidos/registros'),
+    })
+  ],
 });
